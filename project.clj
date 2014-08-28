67e656a7c5d45b5fa7c7ef8a5aedb1dd3a8cc92a
Maurits <maurits.rijk@gmail.com>
2014-08-28 16:31:23 +0200

    Add Esper wrapper

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.espertech/esper "4.9.0" :exclusions [log4j]]
                  [com.rmoquin.bundle/jeromq "0.2.0"]
                  [cheshire "5.3.1"]]
   :main log2esper.core)
